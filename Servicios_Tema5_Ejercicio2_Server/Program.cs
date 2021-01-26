b736afe7c8c575e0e38d37da9cf81c6c5f987b74
Irissf <iris.seijo@gmail.com>
2021-01-26 13:11:00 +0100

    Agregar archivos de proyecto.

---
+++ Servicios_Tema5_Ejercicio2_Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios_Tema5_Ejercicio2_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
