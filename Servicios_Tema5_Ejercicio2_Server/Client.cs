8f59f3aef7b56fe101fc916523f6c9be23ab6db7
Iris SF <iris.kosy@gmail.com>
2021-02-08 12:09:24 +0100

    arreglar esta cosa

--- Servicios_Tema5_Ejercicio2_Server/Client.cs
+++ Servicios_Tema5_Ejercicio2_Server/Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,14 @@ namespace Servicios_Tema5_Ejercicio2_Server
     {
         public string name;
         public Socket socket;
+        private IPEndPoint ipEndpoint;
+        public int port;
 
         public Client(Socket socket)
         {
             this.socket = socket;
+            ipEndpoint = (IPEndPoint)socket.RemoteEndPoint;
+            port = ipEndpoint.Port;
         }
     }
 }
--- Servicios_Tema5_Ejercicio2_Server/Program.cs
+++ Servicios_Tema5_Ejercicio2_Server/Program.cs
@@ -15,11 +15,14 @@ namespace Servicios_Tema5_Ejercicio2_Server
         static string message;
         static object key = new Object();
         static Client clientList;
+        static List<Client> allClietsOnServer;
+        static string nameSpeaker;
 
         static void Main(string[] args)
         {
             int puerto = 31416;
             Thread thread;
+            allClietsOnServer = new List<Client>();
 
             //IPEndPoint => Representa un punto de conexión de red como una dirección IP y un número de puerto.
             IPEndPoint ie = new IPEndPoint(IPAddress.Any, puerto);
@@ -44,7 +47,6 @@ namespace Servicios_Tema5_Ejercicio2_Server
             }
             catch (SocketException e) when (e.ErrorCode == (int)SocketError.AddressAlreadyInUse)
             {
-
                 Console.WriteLine("puerto en uso");
             }
             socket.Close();
@@ -57,7 +59,9 @@ namespace Servicios_Tema5_Ejercicio2_Server
             Socket client = (Socket)socket;
             IPEndPoint ieClient = (IPEndPoint)client.RemoteEndPoint;
             Console.WriteLine("conectado al puerto {0}", ieClient.Port);
+
             clientList = new Client(client);
+            allClietsOnServer.Add(clientList);
 
             using (NetworkStream ns = new NetworkStream(client))
             using (StreamReader sr = new StreamReader(ns))
@@ -66,32 +70,44 @@ namespace Servicios_Tema5_Ejercicio2_Server
                 //mandamos al cliente un mensaje de bienvenida :)
                 sw.WriteLine("Wellcome, who are you");//你好， 你是谁?
                 sw.Flush();
-                
+
                 clientList.name = sr.ReadLine();
-                Console.WriteLine("El usuario conectado al puerto{0}, es {1}",ieClient.Port,clientList.name);
+                Console.WriteLine("the user conect to port{0}, is {1}", ieClient.Port, clientList.name);
                 sw.WriteLine("Welcome {0}.", clientList.name);
                 sw.Flush();
+
                 while (true)
                 {
                     try
                     {
                         message = sr.ReadLine();
+                        nameSpeaker = clientList.name;
+
                         if (message != null)
                         {
-                            sw.WriteLine(clientList.name + ": " + message);
-                            sw.Flush();
+                            Console.WriteLine("name writter {0}", nameSpeaker);
+                            for (int i = 0; i < allClietsOnServer.Count; i++)
+                            {
+                                using (NetworkStream nsInside = new NetworkStream(allClietsOnServer[i].socket))
+                                using (StreamWriter swInside = new StreamWriter(nsInside))
+                                {
+                                    swInside.WriteLine(nameSpeaker + ": " + message);
+                                    swInside.Flush();
+                                    nameSpeaker = "";
+                                }
+
+                            }
+
                         }
                     }
                     catch (IOException e)
                     {
                         Console.WriteLine(e.Message);
                     }
+
                 }
-                
 
             }
-
-
         }
     }
 }
